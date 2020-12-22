17e2d255704bed75a02c6e14b2295254a1a9970e
freshjy <freshjeongyeop@gmail.com>
2020-12-22 11:35:52 +0900

    emaillist02 init

---
+++ emaillist02/src/main/java/com/markany/emaillist/controller/EmaillistController.java
@@ -0,0 +1,44 @@
+package com.markany.emaillist.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.markany.emaillist.dao.EmaillistDao;
+import com.markany.emaillist.vo.EmaillistVo;
+
+
+public class EmaillistController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		
+		String actionName = request.getParameter("a");
+		
+		if("form".equals(actionName)) {
+			
+		}else if("add".equals(actionName)) {
+			
+		}else {
+			List<EmaillistVo> list = new EmaillistDao().findAll();
+			
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
